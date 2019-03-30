64580f3eb3e9fcc0f1d863554c7c71b9b15fc0bb
Luis Thomas <thomasluis555@gmail.com>
2019-03-29 20:34:18 -0400

    Creates TodoForm component. Todos can now be created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 import Todo from "./components/Todo";
-
+import TodoForm from "./components/TodoForm";
 const App = () => {
   const [todos, setTodos] = useState([
     {
@@ -19,12 +19,18 @@ const App = () => {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
